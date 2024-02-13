28229bb0282a96a471583e77d9272c633e6930c8
gabrielQ12 <gabriel.quinette1@gmail.com>
2024-02-13 16:19:57 +0100

    bug

---
+++ Top14Map/Model/Club.swift
@@ -0,0 +1,8 @@
+//
+//  Club.swift
+//  Top14Map
+//
+//  Created by Gaby on 13/02/2024.
+//
+
+import Foundation
---
+++ Top14Map/Model/Datas.swift
@@ -0,0 +1,8 @@
+//
+//  Datas.swift
+//  Top14Map
+//
+//  Created by Gaby on 13/02/2024.
+//
+
+import Foundation
